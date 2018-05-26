9ee2efa80a66b5bb78a46a2c06eaac0d9cb34808
Babubhai <shubhsharma10@gmail.com>
2018-05-26 00:03:35 -0400

    Dropdown widget type

--- src/components/widget.js
+++ src/components/widget.js
@@ -3,6 +3,33 @@ import {connect} from 'react-redux'
 import {addWidget, deleteWidget} from '../actions/index'
 import {SELECT_WIDGET_TYPE} from '../constants/index'
 
+const Heading = () => (
+    <div>
+        <h2>Heading</h2>
+        <select>
+            <option>Heading 1</option>
+            <option>Heading 2</option>
+            <option>Heading 3</option>
+        </select>
+    </div>
+);
+
+const Paragraph = () => (
+    <div>
+        <h2>Paragraph</h2>
+        <textarea></textarea>
+    </div>
+);
+
+const Image = () => (
+    <h2>Image</h2>
+);
+
+const List = () => (
+    <h2>List</h2>
+);
+
+
 const WidgetComponent = ({widget,dispatch}) => {
     let select;
     return(
@@ -27,6 +54,12 @@ const WidgetComponent = ({widget,dispatch}) => {
             }}>
                 Delete
             </button>
+            <div>
+                {widget.widgetType==='Heading' && <Heading/>}
+                {widget.widgetType==='Paragraph' && <Paragraph/>}
+                {widget.widgetType==='List' && <List/>}
+                {widget.widgetType==='Image' && <Image/>}
+            </div>
         </li>
     );
 };
